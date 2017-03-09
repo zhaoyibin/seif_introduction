7a3884870f62c6810e7953b28d0d357215cdce98
zhaoyibin <1668954386@qq.com>
2017-03-09 13:33:08 +0800

    这个首次

---
+++ js/self_introduction.js
@@ -0,0 +1,6 @@
+$("#sina").click(function(){
+    window.location.href="http://weibo.com/u/5544780121?refer_flag=1001030101_&is_hot=1";
+});
+$("#qq").click(function(){
+    window.location.href="http://user.qzone.qq.com/1668954386/main";
+});
